62a03758b55659ab8a8598f7beb168620936ec1e
aswinthomas502 <aswinthomas502@gmail.com>
2024-01-18 11:15:47 +0530

    commit

--- src/app/Authentication/login-screen/login-screen.component.ts
+++ src/app/Authentication/login-screen/login-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-screen',
@@ -7,6 +7,59 @@ import { Component } from '@angular/core';
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.scss'
 })
-export class LoginScreenComponent {
+export class LoginScreenComponent implements OnInit{
+  ngOnInit(): void {
+  }
 
+  isSignin:boolean = true
+  isSignup:boolean = false
+  signin:boolean = false
+  signup:boolean = false
+  signop:boolean = false
+  signp:boolean = false
+
+  signCase : "Sign Up" | "Sign In"|"Get OTP" ="Get OTP"
+
+  ifCase(){
+    if(this.isSignin){
+      this.signin=true
+      this.signup=false
+    }
+    else{
+      this.signup=true
+      this.signin=false
+    }
+  }
+
+
+  butLogin(){
+    this.ifCase()
+    if(this.signin){
+      this.signCase ="Sign In"
+    }
+    else if(this.signop){
+      this.signp=true
+      this.signup=false
+    }
+    else if(this.signup){
+      this.signCase ="Sign Up" 
+      this.signop=true
+    }
+    else{
+      this.signCase ="Get OTP"
+    }
+
+  }
+
+
+  mainLogin(){
+    this.isSignin=true
+    this.isSignup=false
+  }
+
+
+  mainSignup(){
+    this.isSignin=false
+    this.isSignup=true
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginScreenComponent } from './Authentication/login-screen/login-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,LoginScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
