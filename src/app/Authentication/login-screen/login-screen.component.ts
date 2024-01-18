b3bbd91283da91d204ba0b6faab309a262f77eb9
aswinthomas502 <aswinthomas502@gmail.com>
2024-01-18 11:16:21 +0530

    commit

--- src/app/Authentication/login-screen/login-screen.component.ts
+++ src/app/Authentication/login-screen/login-screen.component.ts
@@ -1,24 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-screen',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.scss'
 })
 export class LoginScreenComponent implements OnInit{
   ngOnInit(): void {
+    this.initializeForm()
+  }
+  initializeForm() {
+    this.userform = this.FormBuilder.group({
+      mobno : new FormControl('',Validators.required),
+      // fname : new FormControl('',Validators.required),
+      // email : new FormControl('',[Validators.required, Validators.email]),
+      // gender : new FormControl('',Validators.required),
+      // date : new FormControl('',Validators.required),
+      // state : new FormControl('',Validators.required),
+      otp : new FormControl('',Validators.required)
+    })
   }
 
+
+  private FormBuilder = inject(FormBuilder)
+  userform! : FormGroup
   isSignin:boolean = true
   isSignup:boolean = false
   signin:boolean = false
   signup:boolean = false
   signop:boolean = false
   signp:boolean = false
+  isButtonDisabled = false;
 
   signCase : "Sign Up" | "Sign In"|"Get OTP" ="Get OTP"
+
+  constructor(){
+    this.userform 
+  }
+
 
   ifCase(){
     if(this.isSignin){
@@ -31,7 +58,7 @@ export class LoginScreenComponent implements OnInit{
     }
   }
 
-
+myCase = "getOpt"
   butLogin(){
     this.ifCase()
     if(this.signin){
@@ -48,7 +75,11 @@ export class LoginScreenComponent implements OnInit{
     else{
       this.signCase ="Get OTP"
     }
+  }
 
+
+  validateCase(){
+    return this.userform.invalid
   }
 
 
