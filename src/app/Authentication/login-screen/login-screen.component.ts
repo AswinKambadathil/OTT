684b55e6f57660dac5993999d6c299afc8af7469
aswinthomas502 <aswinthomas502@gmail.com>
2024-01-19 11:30:27 +0530

    commit

--- src/app/Authentication/login-screen/login-screen.component.ts
+++ src/app/Authentication/login-screen/login-screen.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
@@ -14,38 +14,76 @@ import { CommonModule } from '@angular/common';
   styleUrl: './login-screen.component.scss'
 })
 export class LoginScreenComponent implements OnInit{
+
   ngOnInit(): void {
-    this.initializeForm()
-  }
-  initializeForm() {
-    this.userform = this.FormBuilder.group({
-      mobno : new FormControl('',Validators.required),
-      // fname : new FormControl('',Validators.required),
-      // email : new FormControl('',[Validators.required, Validators.email]),
-      // gender : new FormControl('',Validators.required),
-      // date : new FormControl('',Validators.required),
-      // state : new FormControl('',Validators.required),
-      otp : new FormControl('',Validators.required)
-    })
+      this.initializeForm1()
+      this.initializeForm2()
+      this.initializeForm3()
   }
 
+  buttonColor(): string {
+    return  this.isSignin? 'red' : 'white';
+  }
+
+  buttonColor1(): string {
+    return  this.isSignup? 'red' : 'white';
+  }
 
   private FormBuilder = inject(FormBuilder)
-  userform! : FormGroup
+  userform1! : FormGroup
+  userform2! : FormGroup
+  userform3! : FormGroup
+  getOtp:boolean = true
   isSignin:boolean = true
   isSignup:boolean = false
   signin:boolean = false
   signup:boolean = false
-  signop:boolean = false
-  signp:boolean = false
-  isButtonDisabled = false;
+  signUpF:boolean = false
 
-  signCase : "Sign Up" | "Sign In"|"Get OTP" ="Get OTP"
+  signCase : "Sign Up" | "Sign In" = "Sign In"
 
-  constructor(){
-    this.userform 
+
+  initializeForm1() {
+    this.userform1 = this.FormBuilder.group({
+      mobno : new FormControl('',Validators.required)
+    })
   }
 
+
+
+  initializeForm2() {
+    this.userform2 = this.FormBuilder.group({
+      mobno : [''],
+      otp1 : new FormControl('',Validators.required),
+      otp2 : new FormControl('',Validators.required,),
+      otp3 : new FormControl('',Validators.required),
+      otp4 : new FormControl('',Validators.required),
+      otp5 : new FormControl('',Validators.required),
+      otp6 : new FormControl('',Validators.required)
+    })
+  }
+
+
+  initializeForm3() {
+    this.userform3 = this.FormBuilder.group({
+      mobno : [''],
+      fname : new FormControl('',Validators.required),
+      email : new FormControl('',[Validators.required]),
+      gender : new FormControl('',Validators.required),
+      date : new FormControl('',Validators.required),
+      state : new FormControl('',Validators.required)
+    })
+  }
+
+
+  onChange(mobno:string){
+    this.userform2.patchValue({
+      mobno : mobno
+    })
+    this.userform3.patchValue({
+      mobno : mobno
+    })
+  }
 
   ifCase(){
     if(this.isSignin){
@@ -58,28 +96,19 @@ export class LoginScreenComponent implements OnInit{
     }
   }
 
-myCase = "getOpt"
-  butLogin(){
-    this.ifCase()
-    if(this.signin){
-      this.signCase ="Sign In"
-    }
-    else if(this.signop){
-      this.signp=true
-      this.signup=false
-    }
-    else if(this.signup){
-      this.signCase ="Sign Up" 
-      this.signop=true
-    }
-    else{
-      this.signCase ="Get OTP"
-    }
+
+  validateCase1(){
+    return this.userform1.invalid
   }
 
 
-  validateCase(){
-    return this.userform.invalid
+  validateCase2(){
+    return this.userform2.invalid
+  }
+
+
+  validateCase3(){
+    return this.userform3.invalid
   }
 
 
@@ -93,4 +122,43 @@ myCase = "getOpt"
     this.isSignin=false
     this.isSignup=true
   }
+
+
+  buttonLoginF(){
+    this.getOtp = false
+    this.ifCase()
+    if(this.signin && this.isSignin){
+      this.signCase = "Sign In"
+    }
+
+    else{
+      this.signCase = "Sign Up"
+    }
+  }
+
+
+  buttonLoginS(){
+    if(this.signin){
+      this.signin = false
+      alert("Signed In Successfully")
+
+    }
+    else{
+      this.signup = false
+      this.signUpF = true
+    }
+  }
+
+
+  buttonLoginT(){
+    alert("Signed Up Successfully")
+  }
+
+
+  onInputChange(currentInput: HTMLInputElement, nextInput: HTMLInputElement): void {
+    if (currentInput && nextInput && currentInput.value.length === 1) {
+      nextInput.focus();
+    }
+  }
 }
+
