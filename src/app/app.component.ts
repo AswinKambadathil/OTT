e6504509b93964b625bfb98e5bd17594144ec3c8
aswinthomas502 <aswinthomas502@gmail.com>
2024-01-31 15:43:40 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginScreenComponent } from './Authentication/login-screen/login-screen.component';
 import { MainCarouselComponent } from './mCarousel/main-carousel/main-carousel.component';
+import { ContinueWatchComponent } from './ContinueWatch/continue-watch/continue-watch.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,LoginScreenComponent,MainCarouselComponent],
+  imports: [CommonModule, RouterOutlet,LoginScreenComponent,MainCarouselComponent,ContinueWatchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-
 import { MainCarouselComponent } from './mCarousel/main-carousel/main-carousel.component';
 import { LoginScreenComponent } from './Authentication/login-screen/login-screen.component';
+import { ContinueWatchComponent } from './ContinueWatch/continue-watch/continue-watch.component';
 
 export const routes: Routes = [
     {path:'login/maincarousel',component:MainCarouselComponent},
-    {path:'login',component:LoginScreenComponent}
+    {path:'login',component:LoginScreenComponent},
+    {path:'login/continuewatch',component:ContinueWatchComponent}
 ];
