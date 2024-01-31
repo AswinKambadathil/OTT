134fbef053eb16acf4fc662a1020dd91f651d4de
aswinthomas502 <aswinthomas502@gmail.com>
2024-01-31 19:02:34 +0530

    commit

--- src/app/ContinueWatch/continue-watch/continue-watch.component.ts
+++ src/app/ContinueWatch/continue-watch/continue-watch.component.ts
@@ -8,42 +8,43 @@ import { Component, Input } from '@angular/core';
   styleUrl: './continue-watch.component.scss'
 })
 export class ContinueWatchComponent {
-  @Input() styleChange : "1stStyle" | "2ndStyle" = "1stStyle"
+  
+  @Input() styleChange: "1stStyle" | "2ndStyle" = "1stStyle"
   imageArray = [
     {
       imageUrl: 'assets/Login/Movies/kantara-Banner.jpg',
       description: 'Movie',
       title: '#1 in Trending',
       discriptUrl: 'assets/Login/Movies/kantara-Name.jpg',
-      logoUrl:'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
+      logoUrl: 'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
     },
     {
       imageUrl: 'assets/Login/Movies/neru.jpg',
       description: 'Movie',
       title: '#1 in Trending',
       discriptUrl: 'assets/Login/Movies/neru-name.jpeg',
-      logoUrl:'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
+      logoUrl: 'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
     },
     {
       imageUrl: 'assets/Login/Movies/kantara-Banner.jpg',
       description: 'Movie',
       title: '#1 in Trending',
       discriptUrl: 'assets/Login/Movies/kantara-Name.jpg',
-      logoUrl:'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
+      logoUrl: 'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
     },
     {
       imageUrl: 'assets/Login/Movies/ozler.jpg',
       description: 'Movie',
       title: '#1 in Trending',
       discriptUrl: 'assets/Login/Movies/ozler-name.jpeg',
-      logoUrl:'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
+      logoUrl: 'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
     },
     {
       imageUrl: 'assets/Login/Movies/kantara-Banner.jpg',
       description: 'Movie',
       title: '#1 in Trending',
       discriptUrl: 'assets/Login/Movies/kantara-Name.jpg',
-      logoUrl:'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
+      logoUrl: 'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
     }
     ,
     {
@@ -51,7 +52,13 @@ export class ContinueWatchComponent {
       description: 'Movie',
       title: '#1 in Trending',
       discriptUrl: 'assets/Login/Movies/animal-name.jpg',
-      logoUrl:'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
+      logoUrl: 'assets/Login/Movies/Amazon-Prime-Video-Icon.png'
     }
   ];
+
+
+  transformImg() {
+    let mainImageWrap = document.getElementById("mainImageWrap") as HTMLDivElement
+    mainImageWrap.scrollLeft += 300
+  }
 }
