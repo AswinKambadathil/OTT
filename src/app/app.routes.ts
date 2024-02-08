ed70d9baea9bfce9a8f0e43d5248776ef393d21b
athulbabu835 <athulbabu835@gmail.com>
2024-02-08 15:58:30 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { welcomescreen_router } from './Authentication/welcomescreen/welcomescreen.routes';
 import { ProfilepageComponent } from './Authentication/profilepage/profilepage.component';
 import { ChooseLanguageComponent } from './Authentication/choose-language/choose-language.component';
+import { ChoosePlanComponent } from './Authentication/choose-plan/choose-plan.component';
 export const routes: Routes = [
     welcomescreen_router,
     {path:'',redirectTo:'/welcome',pathMatch:'full'},
     {path:'profile',component:ProfilepageComponent},
     {path:'language',component:ChooseLanguageComponent},
+    {path:'chooseplan',component:ChoosePlanComponent}
 ];
