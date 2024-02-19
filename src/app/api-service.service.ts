d3cc33f2e113a13f392062afa8280ac7468f440a
athulbabu835 <athulbabu835@gmail.com>
2024-02-19 11:04:50 +0530

    commit

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 
 @Injectable({
@@ -14,8 +14,15 @@ export class ApiServiceService {
   constructor() {}
 
 
+  getProfileList(): Observable<any[]> {
+    return this.http.get<any>('https://app.pishow.tv/acms/subscriber/getProfilesListBySubscriber/630383ffbf448c47a0a81413')
+      .pipe(
+        map((response: { data: any; }) => response.data) 
+      );
+  }
   getData(): Observable<any> {
     const url = 'https://api.postman.com/collections/23825578-5e314afb-9f4c-4082-bbe3-c7e9fc28ac2b?access_key=PMAT-01HNYB81PCF7DD69XWSNM90V16';
     return this.http.get(url);}
 
-  }
+
+}
